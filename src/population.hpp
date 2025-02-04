b6cb1439c7b12b2cd275bd43a2b2109fceab6781
João Duarte Colares <95177870+joao314159@users.noreply.github.com>
2025-02-04 10:41:31 -0300

    Update population.hpp

--- src/population.hpp
+++ src/population.hpp
@@ -15,11 +15,24 @@ class Population{
     Population(){
         cout<<"TESTE"<<endl<<endl;
     }
-    
+
     Solution Gerar_filho(int index1, int index2){
 
         Solution solution1 = this->solutions[index1];
         Solution solution2 = this->solutions[index2];
+
+        Instance instance = solution1.instance;
+
+        Solution solution3(instance);
+
+        
+        for(int i =0; i< instance.quant_Grup;i++){
+            Grupo grupo;
+            grupo.tam_minimo = solution1.solucao[i].tam_minimo;
+            grupo.tam_maximo = solution1.solucao[i].tam_maximo;
+
+            solution3.solucao.push_back(grupo);
+        }
 
     }
 
@@ -30,11 +43,6 @@ class Population{
     Solution seleciona_e_gera_filho(){
 
 
-
     }
-
-
-
-
 
 };
